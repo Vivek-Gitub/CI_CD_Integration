7b2c18f831e8513ed6585f7bd3054a1aa5be6d9d
Vivek patel <115970183+Vivek-Gitub@users.noreply.github.com>
2024-05-23 16:03:53 +0530

    Revert "Test tutorial for CI unit tseting Setup"

--- Pods/Target Support Files/Pods-CI_CD_Integration-CI_CD_IntegrationUITests/Pods-CI_CD_Integration-CI_CD_IntegrationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CI_CD_Integration_CI_CD_IntegrationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CI_CD_Integration_CI_CD_IntegrationUITests
-@end
--- Pods/Target Support Files/Pods-CI_CD_Integration/Pods-CI_CD_Integration-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CI_CD_Integration : NSObject
-@end
-@implementation PodsDummy_Pods_CI_CD_Integration
-@end
